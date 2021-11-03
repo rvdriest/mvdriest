c3541c9bf79f1c16332868c65e92021d48fe4f2c
rvdriest <richard.vandriest@outlook.com>
2021-11-03 16:30:02 +0100

    Test / route

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,9 +6,6 @@ export default {
 
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
-  router: {
-    base: '/mvdriest/',
-  },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
