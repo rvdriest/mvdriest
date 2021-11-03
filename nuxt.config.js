301d1b0e076f8c37a8a238fbc3857885657f2e7e
rvdriest <richard.vandriest@outlook.com>
2021-11-03 16:35:22 +0100

    Adding /mvdriest route for production

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,6 +6,9 @@ export default {
 
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
+  router: {
+    base: process.env.NODE_ENV === 'production' ? '/mvdriest/' : '/',
+  },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
